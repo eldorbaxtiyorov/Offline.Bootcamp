14003183827d89fe5c8f08d85d4de27cbe151840
Wahid Abduhakimov <wakhid2802@gmail.com>
2024-10-30 21:28:54 +0500

    Delegates p.1

---
+++ Day13/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Input? input = Console.ReadLine;
+var name = input();
+Console.WriteLine(name);
+input -= Console.ReadLine;
+
+if(input is not null)
+    input();
+
+name = input?.Invoke() ?? "null";
+input += IsmimniQaytar;
+
+var eshmat = new Student();
+
+eshmat.StudentPrinter = PrintTalaba;
+
+
+string? PrintTalaba(Student student)
+{
+    return "Mana  shu student";
+}
+
+string? IsmimniQaytar()
+{
+    return "Wahid ustoz";
+}
+
+public class Student
+{
+    public delegate string? PrintStudent(Student student);
+    public PrintStudent? StudentPrinter { get; set; }
+}
+
+
+public delegate string? Input();
